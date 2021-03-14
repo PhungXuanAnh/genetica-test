8855cbbd8d1c8f7a42a7d2fd73f242c2131a184f
Phung Xuan Anh <phungxuananh1991@gmail.com>
2021-03-15 01:23:46 +0700

    inline admin

--- gene/admin.py
+++ gene/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from gene.models import Gene, GeneActivity
 
+
+class GeneActivityAdmin(admin.ModelAdmin):
+    fields = ['gene_sample', 'type', "created_by", "attempts"]
+    list_display = ("gene_sample", "type", "created_by", "attempts")
+
+class GeneActivityInline(admin.StackedInline):
+    model = GeneActivity
+    extra = 0
+
 class GeneAdmin(admin.ModelAdmin):
     fields = ['location', 'processing_status']
     list_display = ("id", "location", "processing_status")
+    inlines = [GeneActivityInline]
+
 
 admin.site.register(Gene, GeneAdmin)
+admin.site.register(GeneActivity, GeneActivityAdmin)
